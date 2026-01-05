fcea5fbee653cb7cd489db2489eaae086abb5c02
HmonWutt <wtthumon@gmail.com>
2026-01-05 16:30:38 +0100

    edited file to file_path for isdir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,24 +8,21 @@ def get_files_info(working_directory, directory="."):
         valid_target_dir = os.path.normpath(os.path.commonpath(
             [working_dir_abs, target_dir])) == working_dir_abs
         if valid_target_dir:
-            output = [f"Result for current directory: "]
-            try:
-                files = os.listdir(target_dir)
-                for file in files:
-                    file_path = os.path.normpath(
-                        os.path.join(target_dir, file))
-                    file_info = f'- {file}: file_size={
-                        os.path.getsize(file_path)} bytes, is_dir={
-                        os.path.isdir(file)}'
-                    output.append(file_info)
-                return "\n".join(output)
+            output = ["Result for current directory: "]
+            files = os.listdir(target_dir)
+            for file in files:
+                file_path = os.path.normpath(
+                    os.path.join(target_dir, file))
+                file_info = f'- {file}: file_size={
+                    os.path.getsize(file_path)} bytes, is_dir={
+                    os.path.isdir(file_path)}'
+                output.append(file_info)
+            return "\n".join(output)
 
-            except BaseException:
-                return f'Error: File path error'
         else:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     except BaseException:
         return f'Error: "{directory}" is not a directory'
 
 
-print(get_files_info("pkg"))
+print(get_files_info("calculator"))
